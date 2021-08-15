893777d7c80d5d93e5421b5ad47696dc6e799c7f
arshad54 <rizwanhiroli@gmail.com>
2021-08-15 19:28:59 +0530

    newly added

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::group(['namespace' => 'App\Http\Controllers'], function () {
     Route::resource('/posts', 'PostsController');
 });
